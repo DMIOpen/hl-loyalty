78bb7e9e7a62e21f328cdfe6abad77631db20db2
Vikas Agarwal <vikasagarwal@dmis-MacBook-Pro.local>
2018-03-21 23:34:23 +0530

    Initial commit

---
+++ hl-loyalty/lib/logic.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {com.dmi.blockchain.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('com.dmi.blockchain.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
